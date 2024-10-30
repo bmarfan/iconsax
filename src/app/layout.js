82fa4e436d2e375a0a220e348e5acd3cd5ecf587
Bernardita Marfán <bmmarfan@gmail.com>
2024-10-30 03:33:31 -0300

    upload app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,7 @@
-import localFont from "next/font/local";
+import {NextUIProvider} from "@nextui-org/react";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -20,11 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <NextUIProvider>
+      <body>
         {children}
       </body>
+      </NextUIProvider>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,86 @@
-import Image from "next/image";
+'use client'
+import icons from "@/components/IconList";
+import '../app/fonts/style.css'
+import iconLinear from "./data/linear";
+import iconsBold from "./data/bold";
+import iconBroken from "./data/broken";
+import { Tabs, Tab } from "@nextui-org/tabs";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <main className="flex justify-center">
+            <div className="flex w-full flex-col">
+                <Tabs aria-label="Options" color="secondary" fullWidth>
+                    <Tab key="linear" title="Linear">
+                        <Card> 
+                            <CardBody>
+                                <section className="flex flex-wrap gap-2 tab" id="linear">
+                                    {
+                                        iconLinear.map((x, index) => (
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+                                            <article key={index} className="flex flex-col justify-center items-center w-[12%] p-10 bg-gray-50 rounded-lg border border-black/10">
+                                                <div className="text-[25px]">
+                                                    <i className="iconsax" icon-name={x}></i>
+                                                </div>
+                                                <div className="text-[11px] text-center w-36">
+                                                    <span>{x}</span>
+                                                </div>
+                                            </article>
+
+                                        ))
+                                    }
+                                </section>
+                            </CardBody>
+                        </Card>
+                    </Tab>
+                    <Tab key="broken" title="Broken">
+                        <Card>
+                            <CardBody>
+                                <section className="flex flex-wrap gap-2 tab" id="linear">
+                                    {
+                                        iconBroken.map((x, index) => (
+
+                                            <article key={index} className="flex flex-col justify-center items-center w-[12%] p-10 bg-gray-50 rounded-lg border border-black/10">
+                                                <div className="text-[25px]">
+                                                    <i className="iconsax" icon-name={x} variant="broken"></i>
+                                                </div>
+                                                <div className="text-[11px] text-center w-36">
+                                                    <span>{x}</span>
+                                                </div>
+                                            </article>
+
+                                        ))
+                                    }
+                                </section>
+                            </CardBody>
+                        </Card>
+                    </Tab>
+                    <Tab key="bold" title="Bold">
+                        <Card>
+                            <CardBody>
+                                <section className="flex flex-wrap gap-2 tab" id="bold">
+                                    {
+                                        iconsBold.map((x, index) => (
+
+                                            <article key={index} className="flex flex-col justify-center items-center w-[12%] p-10 bg-gray-50 rounded-lg border border-black/10">
+                                                <div className="text-[25px]">
+                                                    <i className="iconsax" icon-name={x} variant="bold"></i>
+                                                </div>
+                                                <div className="text-[11px] text-center w-36">
+                                                    <span>{x}</span>
+                                                </div>
+                                            </article>
+
+                                        ))
+                                    }
+                                </section>
+                            </CardBody>
+                        </Card>
+                    </Tab>
+                </Tabs>
+            </div>
+
+        </main>
+    )
 }
